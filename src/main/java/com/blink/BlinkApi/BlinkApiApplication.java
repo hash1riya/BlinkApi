b9e88466c2dba88cb8d35d7c266fc2d272f939ca
Daniil Drok <daniildrok1@gmail.com>
2026-05-27 15:55:03 +0200

    init

---
+++ src/main/java/com/blink/BlinkApi/BlinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.blink.BlinkApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlinkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blink/BlinkApi/BlinkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blink.BlinkApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlinkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
